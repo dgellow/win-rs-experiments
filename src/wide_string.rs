66d71b536c9d47132ee3588172808d974a628d5a
Samuel El-Borai <sam@elborai.me>
2022-01-10 21:10:37 +0100

    Implement ToWide for String

--- src/wide_string.rs
+++ src/wide_string.rs
@@ -15,6 +15,14 @@ impl ToWide for &str {
 	}
 }
 
+impl ToWide for String {
+	fn to_wide(&self) -> WideString {
+		let mut result: Vec<u16> = self.encode_utf16().collect();
+		result.push(0);
+		WideString(result)
+	}
+}
+
 impl WideString {
 	pub fn as_pwstr(&self) -> PWSTR {
 		PWSTR(self.0.as_ptr() as *mut _)
