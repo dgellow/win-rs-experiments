d7736fe4d074bc8d83f78a312519e3c6d6b64b6d
Samuel El-Borai <sam@elborai.me>
2022-01-10 12:28:28 +0100

    Link to win32 error code

--- src/assert.rs
+++ src/assert.rs
@@ -1,7 +1,26 @@
-use windows::Win32::Foundation::GetLastError;
+use windows::Win32::Foundation::{GetLastError, WIN32_ERROR};
 
 pub type Error = Box<dyn std::error::Error + Sync + Send>;
 pub type Result<T> = std::result::Result<T, Error>;
+
+fn win32_error_codes_url(win_err: WIN32_ERROR) -> String {
+	let base_url: String =
+		"https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes".to_owned();
+	let url = match win_err {
+		0..=499 => base_url + "--0-499-",
+		500..=999 => base_url + "--500-999-",
+		1000..=1299 => base_url + "--1000-1299-",
+		1300..=1699 => base_url + "--1300-1699-",
+		1700..=3999 => base_url + "--1700-3999-",
+		4000..=5999 => base_url + "--4000-5999-",
+		6000..=8199 => base_url + "--6000-8199-",
+		8200..=8999 => base_url + "--8200-8999-",
+		9000..=11999 => base_url + "--9000-11999-",
+		12000..=15999 => base_url + "--12000-15999-",
+		_ => base_url,
+	};
+	format!("{}?code={}", url, win_err)
+}
 
 pub trait WithLastWin32Error<T> {
 	fn with_last_win32_err(self) -> Result<T>;
@@ -14,10 +33,11 @@ impl<T> WithLastWin32Error<T> for Result<T> {
 			Err(e) => {
 				let win_err = unsafe { GetLastError() };
 				Err(format!(
-					"{}\n\t→ last win32 error: {:#?} ({:#X}). See https://docs.microsoft.com/en-us/windows/win32/debug/system-error-codes.",
+					"{}\n\t→ Last win32 error: {:#?} ({:#X}).\n\t  See {}.",
 					e.to_string(),
 					win_err,
-					win_err
+					win_err,
+					win32_error_codes_url(win_err),
 				)
 				.into())
 			}
